2e885d4e9c93dfc556fdae17e9e71217ee56db50
Jonathan Wage <jwage@bambino.local>
2009-11-20 10:54:12 -0500

    -

--- lib/DoctrineExtensions/REST/EntityManager/WrapperInterface.php
+++ lib/DoctrineExtensions/REST/EntityManager/WrapperInterface.php
@@ -5,7 +5,9 @@ namespace DoctrineExtensions\REST\EntityManager;
 interface WrapperInterface
 {
     public function createQueryBuilder();
+    public function createQuery();
     public function getMetadataFactory();
+    public function persist($entity);
     public function remove($entity);
     public function flush();
     public function find($entity, $id);
